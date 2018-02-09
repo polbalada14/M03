9d434135f3227d360f90210638122091fcb75750
polbalada14 <36170280+polbalada14@users.noreply.github.com>
2018-02-09 15:40:02 +0100

    Add files via upload

---
+++ .gitignore/Python/Exercicis/ejercicio-mayor.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# Autor: Pol Balada
+# 9/2/18
+
+valor1=input("Introdueix el primer numero: ")
+valor2=input("Introdueix el segon numero: ")
+
+if valor1>valor2 :
+    print"El primer numero es mes gran"
+else :
+    if valor2>valor1 :
+        print"El segon numero es mes gran"
+    else :
+	    print"Es el mateix numero"
