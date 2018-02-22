c67146832c5139ab3626e7fb08f2851c0326ace2
polbalada14 <36170280+polbalada14@users.noreply.github.com>
2018-02-22 10:37:49 +0000

    Update gasolinera.py

--- .gitignore/Python/Exercicis/gasolinera.py
+++ .gitignore/Python/Exercicis/gasolinera.py
@@ -2,6 +2,7 @@
 # Autor: Pol Balada
 # 21/2/18
 
+# Mostrem la llista de preus
 print """Bienvenido a la gasolinera,
 ¿que tipo de gasolina quiere?
 
@@ -11,12 +12,14 @@ print """Bienvenido a la gasolinera,
 
 tipo_gasolina=input("Introduzca tipo de gasolina: ")
 
+# Si escull gasolina SUPER (1) li preguntem quin tipus de SUPER
 if (tipo_gasolina==1) :
     print "Ha elegido Gasolina Super"
     print """Que tipo de Gasolina super quiere?
     Escriva 1 para Normal (1,50€/l)
     Escriva 2 para Turbo (1,55€/l)"""
     tipo_super=input("Escoga tipo de Gasolina super: ")
+# Preguntem el tipus de gasolina SUPER, demanem els litres que voldrà i els multipliquem per el preu, ho mostrem per pantalla
     if (tipo_super==1) :
         print "Ha escogido Gasolina Super Normal"
         litros_super_normal=input("Introduzca los litros que desea comprar: ")
@@ -25,14 +28,19 @@ if (tipo_gasolina==1) :
         print "Ha escogido Gasolina Super Turbo"
         litros_super_turbo=input("Introduzca los litros que desea comprar: ")
         print "Ha introducido", litros_super_turbo , "l, el precio es de ", litros_super_turbo*1.55, "$"
+# Si escriu un numero erroni de gasolina, enviem missatge d'error    else :
     else :
         print "Ha introducido un numero erroneo."
+
+# Si escull gasolina SIN PLOMO (2) li preguntem quin tipus de SIN PLOMO
+
 elif (tipo_gasolina==2) :
     print "Ha elegido Gasolina Sin Plomo"
     print """Que tipo de Gasolina Sin Plomo quiere?
     Escriva 1 para Normal (1,60€/l)
     Escriva 2 para Con aditivo de sabor naranja (1,65€/l)"""
     tipo_super=input("Escoga tipo de Gasolina Sin Plomo: ")
+# Preguntem el tipus de gasolina SUPER, demanem els litres que voldrà i els multipliquem per el preu, ho mostrem per pantalla
     if (tipo_super==1) :
         print "Ha escogido Gasolina Sin Plomo Normal"
         litros_sinplomo_normal=input("Introduzca los litros que desea comprar: ")
@@ -41,14 +49,19 @@ elif (tipo_gasolina==2) :
         print "Ha escogido Gasolina Super Turbo"
         litros_sinplomo_add=input("Introduzca los litros que desea comprar: ")
         print "Ha introducido", litros_sinplomo_add , "l, el precio es de ", litros_sinplomo_add*1.65, "$"
+# Si escriu un numero erroni de gasolina, enviem missatge d'error    else :
     else :
         print "Ha introducido un numero erroneo."
+
+# Si escull gasolina DIESEL (3) li preguntem quin tipus de DIESEL
+
 elif (tipo_gasolina==3) :
     print "Ha elegido Gasolina Diesel"
     print """Que tipo de Gasolina Diesel quiere?
     Escriva 1 para Normal (1,70€/l)
     Escriva 2 para Fast&Fourius (1,75€/l)"""
     tipo_super=input("Escoga tipo de Gasolina Diesel: ")
+# Preguntem el tipus de gasolina SUPER, demanem els litres que voldrà i els multipliquem per el preu, ho mostrem per pantalla
     if (tipo_super==1) :
         print "Ha escogido Gasolina Diesel Normal"
         litros_diesel_normal=input("Introduzca los litros que desea comprar: ")
@@ -57,7 +70,11 @@ elif (tipo_gasolina==3) :
         print "Ha escogido Gasolina Diesel Fast"
         litros_diesel_fast=input("Introduzca los litros que desea comprar: ")
         print "Ha introducido", litros_diesel_fast , "l, el precio es de ", litros_diesel_fast*1.75, "$"
-    else :
+# Si escriu un numero erroni de gasolina, enviem missatge d'error    else :
         print "Ha introducido un numero erroneo."
+
+# Si escriu un numero erroni de gasolina, enviem missatge d'error
+
 else:
     print "Ha introducido un numero erroneo."
+
